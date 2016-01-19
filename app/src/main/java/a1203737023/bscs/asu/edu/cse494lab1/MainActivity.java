2c50a06ab5120542cd2888df4126025fef19e5e0
Chance Nursey-Bush <cnurseyb@asu.edu>
2016-01-19 10:22:18 -0700

    CSE494 Project1 click button to generate movie and show string in textview

--- app/src/main/java/a1203737023/bscs/asu/edu/cse494lab1/MainActivity.java
+++ app/src/main/java/a1203737023/bscs/asu/edu/cse494lab1/MainActivity.java
@@ -6,20 +6,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView jsonTV;
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    String movieJson;
+    Movie m;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        jsonTV = (TextView)findViewById(R.id.jsonText);
-    }
+        textView = (TextView) findViewById(R.id.textView);
 
-    public void buttonClicked(View v) {
-        //Log.w(this.getClass().getSimpleName(), "Ouch");
-        String movieJson = "{\"Title\":\"Memento\",\"Year\":\"2000\",\"Rated\":\"R\",\"Released\":\"25 May 2001\",\"Runtime\":\"113 min\",\"Genre\":\"Mystery, Thriller\", \"Actors\":\"Guy Pearce, Carrie-Anne Moss, Joe Pantoliano, Mark Boone Junior\",\"Plot\":\"A man creates a strange system to help him remember things; so he can hunt for the murderer of his wife without his short-term memory loss being an obstacle.\"}";
-        Movie m = new Movie(movieJson);
-        jsonTV.setText(m.toJsonString());
+        movieJson = "{\"Title\":\"Memento\",\"Year\":\"2000\",\"Rated\":\"R\",\"Released\":\"25 May 2001\",\"Runtime\":\"113 min\",\"Genre\":\"Mystery, Thriller\", \"Actors\":\"Guy Pearce, Carrie-Anne Moss, Joe Pantoliano, Mark Boone Junior\",\"Plot\":\"A man creates a strange system to help him remember things; so he can hunt for the murderer of his wife without his short-term memory loss being an obstacle.\"}";
+        m = new Movie(movieJson);
+
+    }
+    public void onButtonClick(View v) {
+        textView.setText(m.toJsonString());
     }
 }
--- app/src/main/java/a1203737023/bscs/asu/edu/cse494lab1/Movie.java
+++ app/src/main/java/a1203737023/bscs/asu/edu/cse494lab1/Movie.java
@@ -18,6 +18,70 @@ public class Movie {
     public String actors;
     public String plot;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getRated() {
+        return rated;
+    }
+
+    public void setRated(String rated) {
+        this.rated = rated;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
     Movie(String jsonString) {
         try {
             JSONObject jo = new JSONObject(jsonString);
